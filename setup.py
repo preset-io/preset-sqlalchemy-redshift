dce67b9cd6782d68711f0514404eae3f15d150db
Beto Dealmeida <roberto@dealmeida.net>
2023-08-24 10:41:12 -0700

    Add custom suffix

--- setup.py
+++ setup.py
@@ -4,17 +4,17 @@ readme = open('README.rst').read()
 history = open('CHANGES.rst').read().replace('.. :changelog:', '')
 
 setup(
-    name='sqlalchemy-redshift',
+    name='preset-sqlalchemy-redshift',
     version='0.8.14',
-    description='Amazon Redshift Dialect for sqlalchemy',
+    description='Fork of Amazon Redshift Dialect for sqlalchemy with custom dialect suffix',
     long_description=readme + '\n\n' + history,
     long_description_content_type='text/x-rst',
     author='Matt George',
     author_email='mgeorge@gmail.com',
-    maintainer='Thomas Grainger',
-    maintainer_email='sqlalchemy-redshift@graingert.co.uk',
+    maintainer='Beto Dealmeida',
+    maintainer_email='beto@preset.io',
     license="MIT",
-    url='https://github.com/sqlalchemy-redshift/sqlalchemy-redshift',
+    url='https://github.com/preset-io/preset-sqlalchemy-redshift',
     packages=['sqlalchemy_redshift', 'redshift_sqlalchemy'],
     package_data={'sqlalchemy_redshift': ['redshift-ca-bundle.crt']},
     python_requires='>=3.4',
@@ -42,10 +42,10 @@ setup(
     ],
     entry_points={
         'sqlalchemy.dialects': [
-            'redshift = sqlalchemy_redshift.dialect:RedshiftDialect_psycopg2',
-            'redshift.psycopg2 = sqlalchemy_redshift.dialect:RedshiftDialect_psycopg2',
-            'redshift.psycopg2cffi = sqlalchemy_redshift.dialect:RedshiftDialect_psycopg2cffi',
-            'redshift.redshift_connector = sqlalchemy_redshift.dialect:RedshiftDialect_redshift_connector',
+            'redshift.spectrum = sqlalchemy_redshift.dialect:RedshiftDialect_psycopg2',
+            #'redshift.psycopg2 = sqlalchemy_redshift.dialect:RedshiftDialect_psycopg2',
+            #'redshift.psycopg2cffi = sqlalchemy_redshift.dialect:RedshiftDialect_psycopg2cffi',
+            #'redshift.redshift_connector = sqlalchemy_redshift.dialect:RedshiftDialect_redshift_connector',
         ]
     },
 )
